a12bcac2a196fd8b0053be685bfc5e17e06afdd8
andre <andresferreira@deloitte.pt>
2016-07-28 23:17:43 +0100

    Initial commit

---
+++ src/main/java/com/jpm/supersimplestocks/SuperSimpleStocksMain.java
@@ -0,0 +1,13 @@
+package com.jpm.supersimplestocks;
+
+/**
+ * Hello world!
+ *
+ */
+public class SuperSimpleStocksMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
